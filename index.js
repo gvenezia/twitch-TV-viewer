88d51b809b67a7e2d398304f69a0464b972b560b
gvenezia <g.venezia@outlook.com>
2018-02-23 00:38:45 +0000

    display works and is formatted correctly, styling fixes, background-image doesn't work

--- index.js
+++ index.js
@@ -2,34 +2,30 @@
 (function IIFEfileWrapper(){
   'use strict';
   
-    // =============== Variables ================
+    // ================= Variables ==================
     // JS variables
-    let apiResponse     = {},
-        streamArr       = ['freecodecamp', 'ESL_SC2'];
+    let apiResponse = {},
+        streamArr   = ['freecodecamp', 'ESL_SC2'];
     
     // HTML elements
-    const doc = 0;
+    const channelsContainer = document.getElementById('channels-container');
     
     // =============== On Page Load ================
     document.addEventListener('DOMContentLoaded', function DOMLoaded(){
-        streamArr.forEach((element) => {apiCall(element)});
+        
+        streamArr.forEach((streamId) => { apiCall(streamId); });
         
     }, {once: true});
 
 
-    // ============== Functions ================
+    // ================ Functions ==================
     function apiCall(streamId){
         let xhttp = new XMLHttpRequest();
         let url   = `https://wind-bow.glitch.me/twitch-api/streams/${streamId}`;
-        console.log(url);
         
         xhttp.onreadystatechange = function () {
             if (xhttp.readyState == 4 && xhttp.status == 200) {
-                apiResponse = JSON.parse(this.responseText);
-                
-                console.log(apiResponse);
-                
-                displayCurrentStream();
+                displayCurrentStream(streamId, JSON.parse(this.responseText));
                 
             } 
         };
@@ -38,8 +34,41 @@
         xhttp.send();
     };
     
-    function displayCurrentStream() {
-        console.log('displayStreams() called');
-    };
+    function displayCurrentStream(streamId, apiResponse) {
+        // console.log('displayStreams() called');
+        
+        console.log(apiResponse);
+        
+        let streamLink = `https://www.twitch.tv/${streamId}`;
+        
+        let newDiv = document.createElement('div');
+        
+        newDiv.classList.add('row');
+        newDiv.classList.add('stream-div');
+        
+        if (apiResponse.stream != null){
+            newDiv.innerHTML = `<div class="col-2">
+                                    <a href="${streamLink}">
+                                        ${apiResponse.stream.channel.display_name}
+                                    </a>
+                                </div>
+                                <div class="col-6">
+                                    ${apiResponse.stream.channel.status}
+                                </div>
+                                `;
+        } else {
+            newDiv.innerHTML = `<div class="col-2">
+                                    <a href="${streamLink}">
+                                        ${streamId}
+                                    </a>
+                                </div>
+                                <div class="col-6 m-auto">
+                                    <em>Offline</em>
+                                </div>
+                                `;
+        }
+        
+        channelsContainer.appendChild(newDiv);
+    }; // End displayCurrentStream()
     
 })();
